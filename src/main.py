a71961b2aaf7a7a0042e742257dd30860508ba21
hong <j50508@gmail.com>
2023-07-14 09:46:44 +0800

    Add test codes

--- src/main.py
+++ src/main.py
@@ -7,3 +7,12 @@ for item in result:
 
 # かな: kana 'カナ', hiragana: 'かな', romaji: 'kana'
 # 漢字: kana 'カンジ', hiragana: 'かんじ', romaji: 'kanji'
+# 煮込み
+# 二次元nijigen
+# 教えてくれ、五飛
+# 早瀬ユウカ
+# 寿司です
+# ムービー
+# movie
+# うなぎ丼
+# 鰻魚飯 (鰻丼)

6c4b1162c340fc736659289ac38cc64a43a56e00
hong <j50508@gmail.com>
2023-07-15 18:35:24 +0800

    Test codes

--- src/main.py
+++ src/main.py
@@ -1,43 +1,23 @@
+import os
+from pathlib import Path
 import pykakasi
+
+gohan_project_path = Path.cwd()
+tests_path = str(gohan_project_path) + "/tests"
+test_file = tests_path + "/self_test.txt"
+
+f = open(test_file, 'r')
+lines = f.readlines()
+print(f)
+print(lines)
+
 kks = pykakasi.kakasi()
-text = "煮込み"
-result = kks.convert(text)
-for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
-    
-text = "教えてくれ、五飛"
-result = kks.convert(text)
+result = kks.convert(lines)
 for item in result:
     print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
 
-text = "早瀬ユウカ"
-result = kks.convert(text)
-for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
-
-text = "二次元"
-result = kks.convert(text)
-for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
-
-text = "nijigen"
-result = kks.convert(text)
-for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
-
-text = "漬鯊"
-result = kks.convert(text)
-for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn'])) 
-
-# かな: kana 'カナ', hiragana: 'かな', romaji: 'kana'
-# 漢字: kana 'カンジ', hiragana: 'かんじ', romaji: 'kanji'
-# 煮込み
-# 二次元nijigen
-# 教えてくれ、五飛
-# 早瀬ユウカ
-# 寿司です
-# ムービー
-# movie
-# うなぎ丼
-# 鰻魚飯 (鰻丼)
+# kks = pykakasi.kakasi()
+# text = "煮込み"
+# result = kks.convert(text)
+# for item in result:
+#     print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))

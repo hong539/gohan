2770fc150d0cbb16ac94dc2d6f72fac5a32d5234
hong <j50508@gmail.com>
2023-07-15 13:58:40 +0800

    Test codes

--- src/main.py
+++ src/main.py
@@ -23,7 +23,12 @@ for item in result:
 text = "nijigen"
 result = kks.convert(text)
 for item in result:
-    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))    
+    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
+
+text = "漬鯊"
+result = kks.convert(text)
+for item in result:
+    print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn'])) 
 
 # かな: kana 'カナ', hiragana: 'かな', romaji: 'kana'
 # 漢字: kana 'カンジ', hiragana: 'かんじ', romaji: 'kanji'

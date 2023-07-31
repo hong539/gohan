a7e148624715eaa57ed31f51ba4a0bedae812141
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-07-31 23:05:19 +0800

    Update all

--- src/main.py
+++ src/main.py
@@ -4,30 +4,31 @@ import pykakasi
 # from bs4 import BeautifulSoup
 import pandas as pd
 
-gohan_project_path = Path.cwd()
-tests_path = str(gohan_project_path) + "/tests"
-# test_file = tests_path + "/self_test.txt"
-test_file = tests_path + "/example.txt"
+def test_pykakasi():
+    gohan_project_path = Path.cwd()
+    tests_path = str(gohan_project_path) + "/tests"
+    # test_file = tests_path + "/self_test.txt"
+    test_file = tests_path + "/example.txt"
 
-f = open(test_file, 'r')
-lines = f.readlines()
-# print(f)
-# print(lines)
+    f = open(test_file, 'r')
+    lines = f.readlines()
+    # print(f)
+    # print(lines)
 
-def transfer(text):
-    kks = pykakasi.kakasi()
-    result = kks.convert(text)
-    for item in result:
-        print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
+    def transfer(text):
+        kks = pykakasi.kakasi()
+        result = kks.convert(text)
+        for item in result:
+            print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
 
-count = 0
-for line in lines:
-    count += 1
-    # print(line.strip())
-    transfer(line.strip())
+    count = 0
+    for line in lines:
+        count += 1
+        # print(line.strip())
+        transfer(line.strip())
 
 
-def parser():
+def web_parser():
         target_table = pd.read_html("https://bluearchive.wikiru.jp/?%E3%82%AD%E3%83%A3%E3%83%A9%E3%82%AF%E3%82%BF%E3%83%BC%E4%B8%80%E8%A6%A7#j5a6cb9f")        
         print("type:", type(target_table))
         print("len:", len(target_table))
@@ -36,4 +37,5 @@ def parser():
         
 
 if __name__ == "__main__":
-        parser()        
+        # web_parser()
+        test_pykakasi()
